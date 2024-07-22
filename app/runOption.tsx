f71fc811230f298bd598c717cbd8dae4d8f6abee
Dav <davanjit@hotmail.com>
2024-07-22 19:49:07 -0400

    corrected running option mistake

--- app/runOption.tsx
+++ app/runOption.tsx
@@ -1,6 +1,6 @@
 //checks to make sure that it is running developer mode or production mode and sets the correct corresponding path
 export default function getPath(initialPath: string){
-    const path = process.env.NODE_ENV === "production" ? '/personal-website' : ''
+    const path = process.env.NODE_ENV === "production" ? '/Personal-Website/' : ''
 
     return path + initialPath
 }
