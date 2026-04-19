48257b346bb1bac0d1b658a4ca5956b7ea93a581
Dav <davanjit@hotmail.com>
2026-04-19 19:56:39 -0400

    updated projects section to include skills

--- app/portfolio/skills.tsx
+++ app/portfolio/skills.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from "next/image"
 import "./skills.scss"
 
 type Skill = {
@@ -11,7 +12,18 @@ type Skill = {
 export default function Skills({ item }: { item: Skill }){
     return(
         <div className="skill" aria-label="company logo">
-            <img className="h-full w-full object-contain" src={item.img} alt="" />
+            {/* <img className="h-full w-full object-contain" src={item.img} alt="" /> */}
+            
+            <div className="relative h-full w-full">
+                <Image 
+                    className="h-full w-full object-contain"
+                    style={{ objectFit: "contain" }} 
+                    src={item.img} 
+                    alt={item.name}
+                    fill
+                />
+            </div>
+            
             {/* <h1 className="text-center">{item.name}</h1> */}
         </div>
     )
